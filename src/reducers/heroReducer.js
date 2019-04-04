a12f3863c41a6ca5e76d64fb1f385e69507554be
heitor.costa <heitorcost@gmail.com>
2019-04-04 18:57:48 -0300

    [CHANGE]
    Sem o erro no Reducer.

--- src/reducers/heroReducer.js
+++ src/reducers/heroReducer.js
@@ -3,12 +3,9 @@ import {DETAIL_HERO, FETCH_HERO} from '../actions/types';
 export default function heroReducer(state = [], action) {
   switch (action.type) {
     case FETCH_HERO:
-      return {
-        ...state,
-        action.heroes
-      };
+      return action.heroes;
     case DETAIL_HERO:
-      return { ...state, action.hero };
+      return action.hero;
     default:
       return state;
   }
